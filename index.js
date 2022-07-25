12c7db148ff1ddb12e6230964f953c02136f12f5
Isabella Beltrame <93558685+ibltrm@users.noreply.github.com>
2022-07-25 17:00:45 -0300

    lastUpdate

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //MODE SWITCHER
+
 const transition = () => {
     document.documentElement.classList.add('transition');
     window.setTimeout(() => {
@@ -18,6 +19,17 @@ checkbox.addEventListener('change', function() {
     }
 })
 
+//KEYBOARD COMMANDS BUTTON 
+let commandBtn = document.querySelector('.btn-commands');
+let content = document.querySelector('.note-content');
+
+commandBtn.addEventListener('click', () => {
+    if (content.style.display === 'none') {
+        content.style.display = 'block'
+    } else {
+        content.style.display = 'none';
+    }
+});
 
 //CALCULATOR
 const calculator = {
@@ -26,7 +38,6 @@ const calculator = {
     firstOperand: null, //store the first operand of the math expressions
     operator: null, // store the operator for an expression
     waitingForSecondOperand: false, //check if both the first operand and the operator have been set. if true, the next numbers entered will create the second operand
-
 };
 
 function outputDigit(digit) {
@@ -39,7 +50,7 @@ function outputDigit(digit) {
         calculator.outputDisplay = outputDisplay === '0' ? digit : outputDisplay + digit;
     }
 
-    console.log(calculator);
+    // console.log(calculator);
 }
 
 function inputDigit(digit) {
@@ -54,7 +65,7 @@ function inputDigit(digit) {
         calculator.displayValue = displayValue === '0' ? digit : displayValue + digit; // the question mark(?) is being used to check if the current value displayed on the calculator is zero. if so, calculator.displayValue is overwritten with whatever digit was clicked.
     }
 
-    console.log(calculator);
+    // console.log(calculator);
 }
 
 function inputDecimal(dot) {
@@ -74,29 +85,6 @@ function inputDecimal(dot) {
     }
 }
 
-function handleOperator(nextOperator) {
-    const firstOperand = calculator.firstOperand;
-    const outputDisplay = calculator.outputDisplay;
-    const operator = calculator.operator;
-    const outputValue = parseFloat(outputDisplay);
-    if (operator && calculator.waitingForSecondOperand) {
-        calculator.operator = nextOperator;
-        console.log(calculator);
-        return;
-    }
-    if (firstOperand == null && !isNaN(outputValue)) {
-        calculator.firstOperand = outputValue;
-    } else if (operator) {
-        const result = calculate(firstOperand, outputValue, operator);
-        calculator.outputDisplay = `${parseFloat(result.toFixed(7))}`;
-        calculator.firstOperand = result;
-    }
-
-    calculator.waitingForSecondOperand = true;
-    calculator.operator = nextOperator;
-    console.log(calculator);
-}
-
 
 function handleOperator(nextOperator) {
     const firstOperand = calculator.firstOperand;
@@ -107,7 +95,7 @@ function handleOperator(nextOperator) {
 
     //If user clicked '=' button
     if (nextOperator === '=') {
-        console.log('= was clicked');
+        // console.log('= was clicked');
 
         //If currently only has single number, do nothing
         if (!isNaN(Number(calculator.outputDisplay)))
@@ -123,12 +111,48 @@ function handleOperator(nextOperator) {
 
     //Remove '=' from calculator.outputDisplay
     calculator.outputDisplay = calculator.outputDisplay.replace('=', '');
-    //Add clicked operator in calculator.outputDisplay
-    calculator.outputDisplay = calculator.outputDisplay + nextOperator;
+
+    // Get last string in `outputDisplay`.
+    const lastString = calculator.outputDisplay.at(-1);
+
+    // Check if last string is operator.
+    let lastStringIsOperator;
+
+    if (lastString === '+') {
+        lastStringIsOperator = true;
+    } else if (lastString === '-') {
+        lastStringIsOperator = true;
+    } else if (lastString === '/') {
+        lastStringIsOperator = true;
+    } else if (lastString === '*') {
+        lastStringIsOperator = true;
+    } else {
+        lastStringIsOperator = false;
+    }
+
+    // If it's 'true', that means both new input from user and last string of `calculator.outputdDisplay` are both operators.
+    if (lastStringIsOperator === true) {
+        // Replace last string of `calculator.outputDisplay` with new operator.
+        const turnOutputDisplayToArray = calculator.outputDisplay.split('');
+
+        // Replace last string with new user input (operator).
+        turnOutputDisplayToArray.splice(calculator.outputDisplay.length - 1, 1, nextOperator);
+
+        // Turn result back into string from array.
+        const finalResultInString = turnOutputDisplayToArray.join('');
+
+        // Assign calculator object with updated value.
+        calculator.outputDisplay = finalResultInString;
+
+    } else {
+        //Add clicked operator in calculator.outputDisplay
+        calculator.outputDisplay = calculator.outputDisplay + nextOperator;
+    }
+
 
     if (operator && calculator.waitingForSecondOperand) {
         calculator.operator = nextOperator;
-        console.log(calculator);
+        // console.log(calculator);
         return;
     }
     //verify that `firstOperand` is null and that the `inputValue` is not a `NaN` value
@@ -144,8 +168,9 @@ function handleOperator(nextOperator) {
 
     calculator.waitingForSecondOperand = true;
     calculator.operator = nextOperator
-    console.log(calculator);
+        // console.log(calculator);
 }
+
 
 function handleFn(fn) {
     const displayValue = calculator.displayValue;
@@ -166,11 +191,13 @@ function handleFn(fn) {
     }
 
     calculator.displayValue = `${parseFloat(result.toFixed(7))}`;
+    calculator.outputDisplay = `${parseFloat(result.toFixed(7))}`;
 
     if (waitingForSecondOperand) {
         calculator.waitingForSecondOperand = false;
     }
 }
+
 
 function calculate(firstOperand, secondOperand, operator) {
     if (operator === '+') {
@@ -184,8 +211,8 @@ function calculate(firstOperand, secondOperand, operator) {
     }
 
     return secondOperand;
-
 }
+
 
 function resetCalculator() {
     calculator.outputDisplay = '0';
@@ -193,12 +220,7 @@ function resetCalculator() {
     calculator.firstOperand = null;
     calculator.waitingForSecondOperand = false;
     calculator.operator = null;
-    console.log(calculator);
-
-    /**
-     * You have to update calculator UI here.
-     * Select the element and reset its values.
-    **/
+    // console.log(calculator);
 }
 
 function updateOutput() {
@@ -211,51 +233,57 @@ updateOutput();
 function updateDisplay() {
     const display = document.querySelector('.display_result'); //select the "display_result" element
 
-  
+    const displayValue = parseFloat(calculator.displayValue);
+    if (!Number.isFinite(displayValue)) {
+        alert('Calculation is out of the accepted range');
+        resetCalculator();
+        return;
+    }
     display.value = calculator.displayValue; // update the value of the element with the content of "displayValue"
 }
 
 updateDisplay();
 
-// KEYBOARD INPUT
-const display = document.querySelector('.display_result');
-document.addEventListener('keydown', (event) => {
-   
-    console.log(event);
-    if (event.key == "0") { 
-      /**
-       * Check if current display value is just '0'.
-       * If it's just '0', don't take any action.
-      **/
-      display.value += event.key; 
-   }
-   /**
-    * Make sure to also update element values.
-   **/
-    if (event.key == "1") { display.value += event.key; }
-    if (event.key == "2") { display.value += event.key; }
-    if (event.key == "3") { display.value += event.key; }
-    if (event.key == "4") { display.value += event.key; }
-    if (event.key == "5") { display.value += event.key; }
-    if (event.key == "6") { display.value += event.key; }
-    if (event.key == "7") { display.value += event.key; }
-    if (event.key == "8") { display.value += event.key; }
-    if (event.key == "9") { display.value += event.key; }
-    if (event.key == "+") {
-      console.log({calculator});
+//KEYBOARD INPUT
+document.addEventListener('keydown', event => {
+    const value = event.key;
+
+    switch (value) {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            handleOperator(value);
+            break;
+        case 'Enter':
+            handleOperator('=');
+            break;
+        case 'Control':
+            handleFn('+/-');
+            break;
+        case '%':
+            handleFn(value);
+            break;
+        case '%':
+            handleFn(value);
+            break;
+        case '.':
+            inputDecimal(value);
+            break;
+        case 'Backspace':
+            resetCalculator();
+            break;
+        default:
+            // check if the key is an integer
+            if (Number.isInteger(parseFloat(value))) {
+                inputDigit(value);
+                outputDigit(value);
+            }
     }
-    if (event.key == "-") {; }
-    if (event.key == "/") {; }
-    if (event.key == "*") {; }
-    if (event.key == "%") {; }
-    if (event.key == "+/-") {; }
-    if (event.key == ".") {; }
-    if (event.key == "=") {; }
-    if (event.key == "Delete") {resetCalculator(); }
-    if (event.key == "Backspace") { display.value = display.value.slice(0, -1); }
+    updateDisplay();
+    updateOutput();
 
-})
-
+});
 
 
 const keys = document.querySelector('.calc_keys'); //to determine what type of key was clicked - whether digits (0-9), operators (+, −, ⨉, ÷, =), decimal point (.) and the reset key (AC) -
@@ -300,4 +328,3 @@ keys.addEventListener('click', event => { //addEventListener method make the doc
     updateOutput();
 
 });
-
